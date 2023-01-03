e612cacd29b004d8071571fe9756603c7a5bad4d
donglee99 <jhnj741@naver.com>
2023-01-03 13:12:39 +0900

    [CHORE] spring 초기 세팅

---
+++ src/main/java/com/team/piickle/PiickleApplication.java
@@ -0,0 +1,13 @@
+package com.team.piickle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiickleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiickleApplication.class, args);
+	}
+
+}
