5da8f30ec73bac67ddeddaab0c056c5a6f947dcf
donglee99 <jhnj741@naver.com>
2023-01-09 14:20:07 +0900

    [FIX] 성별 "기타" 추가

--- src/main/java/com/team/piickle/user/domain/GenderStatus.java
+++ src/main/java/com/team/piickle/user/domain/GenderStatus.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum GenderStatus {
 
     MALE("MALE", "남자"),
-    FEMALE("FEMALE", "여자");
+    FEMALE("FEMALE", "여자"),
+    ETC("ETC", "기타");
 
     private final String status;
     private final String message;
