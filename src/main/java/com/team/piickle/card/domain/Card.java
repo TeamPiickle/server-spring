b4e643aed5509338ebbaccaf8653d1edb65cceb1
Kayoung Yoon <dev.kayoung@gmail.com>
2023-01-23 23:58:53 +0900

    refactor(Card): 카드 엔티티에 카테고리 삭제

--- src/main/java/com/team/piickle/card/domain/Card.java
+++ src/main/java/com/team/piickle/card/domain/Card.java
@@ -17,7 +17,6 @@ import java.util.List;
 @AttributeOverride(name = "id", column = @Column(name = "CARD_ID"))
 @Entity
 public class Card extends BaseEntity {
-
     @Column(name = "CONTENT")
     private String content;
 
@@ -26,7 +25,4 @@ public class Card extends BaseEntity {
 
     @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL)
     private List<CardTag> tags = new ArrayList<>();
-
-    @OneToMany(mappedBy = "card", cascade = CascadeType.ALL)
-    private List<CardCategory> categories = new ArrayList<>();
 }
