d5d14cb9e1f9102219b99c739eba723c00340bad
Kayoung Yoon <dev.kayoung@gmail.com>
2023-01-23 20:45:21 +0900

    fix(Card): Card 서브도메인을 연결테이블로 읽어오기

--- src/main/java/com/team/piickle/card/domain/Card.java
+++ src/main/java/com/team/piickle/card/domain/Card.java
@@ -1,5 +1,8 @@
 package com.team.piickle.card.domain;
 
+import com.team.piickle.card.filter.CardFilter;
+import com.team.piickle.card.tag.CardTag;
+import com.team.piickle.card.category.domain.CardCategory;
 import com.team.piickle.common.domain.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,13 +21,11 @@ public class Card extends BaseEntity {
     @Column(name = "CONTENT")
     private String content;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "FILTER_ID")
-    private List<Filter> filters = new ArrayList<>();
+    @OneToMany(mappedBy = "card", cascade = CascadeType.ALL)
+    private List<CardFilter> filters = new ArrayList<>();
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "TAG_ID")
-    private List<Tag> tags = new ArrayList<>();
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL)
+    private List<CardTag> tags = new ArrayList<>();
 
     @OneToMany(mappedBy = "card", cascade = CascadeType.ALL)
     private List<CardCategory> categories = new ArrayList<>();
