63d5cbf302ac86b37340de5f5bfc38f2f683a4d0
donglee99 <jhnj741@naver.com>
2023-01-25 13:59:39 +0900

    [FEAT] Controller 반환 타입 정의

---
+++ src/main/java/com/team/piickle/util/dto/ErrorResponseDto.java
@@ -0,0 +1,31 @@
+package com.team.piickle.util.dto;
+
+import com.team.piickle.util.StatusCode;
+
+public class ErrorResponseDto extends ResponseDto{
+    private ErrorResponseDto(StatusCode errorCode) {
+        super(false, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    private ErrorResponseDto(StatusCode errorCode, Exception e) {
+        super(false, errorCode.getCode(), errorCode.getMessage(e));
+    }
+
+    private ErrorResponseDto(StatusCode errorCode, String message) {
+        super(false, errorCode.getCode(), errorCode.getMessage(message));
+    }
+
+
+    public static ErrorResponseDto of(StatusCode errorCode) {
+        return new ErrorResponseDto(errorCode);
+    }
+
+    public static ErrorResponseDto of(StatusCode errorCode, Exception e) {
+        return new ErrorResponseDto(errorCode, e);
+    }
+
+    public static ErrorResponseDto of(StatusCode errorCode, String message) {
+        return new ErrorResponseDto(errorCode, message);
+    }
+
+}
