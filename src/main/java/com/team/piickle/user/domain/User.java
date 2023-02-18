0e59b105c752a741d7726cdc361c7a7c2566001c
donglee99 <jhnj741@naver.com>
2023-02-18 14:35:15 +0900

    [FEAT] User 관련 API

--- src/main/java/com/team/piickle/user/domain/User.java
+++ src/main/java/com/team/piickle/user/domain/User.java
@@ -2,6 +2,8 @@ package com.team.piickle.user.domain;
 
 import com.team.piickle.bookmark.domain.Bookmark;
 import com.team.piickle.common.domain.BaseEntity;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
@@ -37,4 +39,27 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Bookmark> bookmarks = new ArrayList<>();
+
+    @Builder
+    public User(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String email, String name, String nickName, GenderStatus gender, String hashedPassword, String profileImageUrl, List<Bookmark> bookmarks) {
+        super(id, createdAt, updatedAt);
+        this.email = email;
+        this.name = name;
+        this.nickName = nickName;
+        this.gender = gender;
+        this.hashedPassword = hashedPassword;
+        this.profileImageUrl = profileImageUrl;
+        this.bookmarks = bookmarks;
+    }
+
+
+    public void update(User nicknameChangedUser) {
+        this.email = nicknameChangedUser.email;
+        this.name = nicknameChangedUser.name;
+        this.nickName = nicknameChangedUser.nickName;
+        this.gender = nicknameChangedUser.gender;
+        this.hashedPassword = nicknameChangedUser.hashedPassword;
+        this.profileImageUrl = nicknameChangedUser.profileImageUrl;
+        this.bookmarks = nicknameChangedUser.bookmarks;
+    }
 }
