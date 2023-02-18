f00782af53ce1048c4cabb163ac5f13771c8a17a
donglee99 <jhnj741@naver.com>
2023-02-18 14:34:31 +0900

    [FIX] BaseEntity @Builder 추가

--- src/main/java/com/team/piickle/common/domain/BaseEntity.java
+++ src/main/java/com/team/piickle/common/domain/BaseEntity.java
@@ -7,15 +7,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
@@ -31,6 +31,7 @@ public class BaseEntity {
     @Column(name = "UPDATED_AT")
     private LocalDateTime updatedAt;
 
+    @Builder
     protected BaseEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.createdAt = createdAt;
