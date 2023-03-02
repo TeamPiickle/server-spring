9d30118bb2a94d4a4440673e7cd96ba3f259465d
donglee99 <jhnj741@naver.com>
2023-03-02 13:39:15 +0900

    [FEAT] 베스트 피클 조회 API

---
+++ src/main/java/com/team/piickle/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,18 @@
+package com.team.piickle.bookmark.repository;
+
+import com.team.piickle.bookmark.domain.Bookmark;
+import com.team.piickle.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    @Query("select bk.card.id from Bookmark bk group by bk.card.id order by count(bk.card.id) DESC")
+    List<Long> findBestCardsId();
+
+    boolean findByUser(User user);
+}
