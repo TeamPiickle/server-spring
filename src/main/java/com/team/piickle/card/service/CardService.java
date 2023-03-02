fff51ab2b14fd478787d8df70df732dfb834198c
donglee99 <jhnj741@naver.com>
2023-03-02 13:39:15 +0900

    [FEAT] 베스트 피클 조회 API

--- src/main/java/com/team/piickle/card/service/CardService.java
+++ src/main/java/com/team/piickle/card/service/CardService.java
@@ -1,8 +1,39 @@
 package com.team.piickle.card.service;
 
+import com.team.piickle.bookmark.repository.BookmarkRepository;
+import com.team.piickle.card.domain.Card;
+import com.team.piickle.card.dto.CardResponseDto;
+import com.team.piickle.card.repository.CardRepository;
+import com.team.piickle.user.domain.User;
+import com.team.piickle.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
-public class CardService {}
+public class CardService {
+
+    private final CardRepository cardRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public List<CardResponseDto> getBestCardList(Long userId) {
+        List<Long> bestCardsId = bookmarkRepository.findBestCardsId();
+        User user = userRepository.findById(userId)
+                .orElseGet(null);
+        List<Card> cardList = cardRepository.findAllByIdList(bestCardsId);
+        List<CardResponseDto> cardResponseDtoList = cardList.stream().map(card -> new CardResponseDto(card))
+                .collect(Collectors.toList());
+        if (!user.equals(null)) {
+            cardResponseDtoList.stream()
+                    .map(card -> card.changeIsBookmarked(bookmarkRepository.findByUser(user)))
+                    .collect(Collectors.toList());
+        }
+        return cardResponseDtoList;
+    }
+}
