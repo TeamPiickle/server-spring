edb35731761183e91ed5ffdf54e02bc0cc10ce36
donglee99 <jhnj741@naver.com>
2023-03-03 15:41:30 +0900

    [FIX] awsConfig 수정

--- src/main/java/com/team/piickle/PiickleApplication.java
+++ src/main/java/com/team/piickle/PiickleApplication.java
@@ -1,13 +1,12 @@
 package com.team.piickle;
 
 import com.team.piickle.auth.jwt.TokenProvider;
-import com.team.piickle.config.AwsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({TokenProvider.class, AwsConfig.class})
+@EnableConfigurationProperties({TokenProvider.class})
 public class PiickleApplication {
 
     static {
--- src/main/java/com/team/piickle/config/AwsConfig.java
+++ src/main/java/com/team/piickle/config/AwsConfig.java
@@ -5,18 +5,22 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
 @Configuration
-@ConstructorBinding
-@ConfigurationProperties(prefix = "cloud.aws.credentials")
+@RequiredArgsConstructor
 public class AwsConfig {
 
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
