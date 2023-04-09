8eced76ef76a9e9dd65932c555a51b4ca24b74ac
Rosie <dev.kayoung@gmail.com>
2023-04-09 12:10:46 +0900

    [REFACTOR] 셀이 비어있는 경우 처리

--- src/main/java/com/team/piickle/card/domain/Card.java
+++ src/main/java/com/team/piickle/card/domain/Card.java
@@ -3,6 +3,7 @@ package com.team.piickle.card.domain;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Document(collection = "cards")
 public class Card {
@@ -20,32 +22,10 @@ public class Card {
     @Transient public static final String SEQUENCE_NAME = "users_sequence";
     @Id private String id;
     private String content;
+    private List<String> filter = new ArrayList<>();
     private List<String> tags = new ArrayList<>();
     private List<ObjectId> category = new ArrayList<>();
-    ;
-    private List<String> filter = new ArrayList<>();
-    ;
     private String createdAt;
     private String updatedAt;
     private long v;
-
-    @Builder
-    public Card(
-            String id,
-            String content,
-            List<String> tags,
-            List<ObjectId> category,
-            List<String> filter,
-            String createdAt,
-            String updatedAt,
-            long v) {
-        this.id = id;
-        this.content = content;
-        this.tags = tags;
-        this.category = category;
-        this.filter = filter;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.v = v;
-    }
 }
