ba5b89a5a075e61d636b68cdac05110e343127b8
Rosie <dev.kayoung@gmail.com>
2023-04-10 23:54:27 +0900

    [FEAT] 어드민 로그인 인터셉터 등록

---
+++ src/main/java/com/team/piickle/auth/admin/AdminMvcConfig.java
@@ -0,0 +1,19 @@
+package com.team.piickle.auth.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+class AdminMvcConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/login");
+    }
+}
